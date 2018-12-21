5fe8c483e9051af3b829a4a7236542e253a7968a
GregRos <life.gregr@gmail.com>
2018-12-22 01:01:54 +0200

    Exported Mats and Mat

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,5 +3,8 @@ import { VecModule } from "./vectors";
 import { VecModuleImpl } from "./impl/vec.impl";
 
 export {Vec, ImplicitVec, VecModule} from "./vectors";
+export {ImplicitMat, Mat} from "./matrix";
 
 export {Vecs} from "./impl/vec.impl";
+
+export {Mats} from "./impl/mat-module.impl";
